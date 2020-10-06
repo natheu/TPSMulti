ddc43070ef868fa0a459db6df64ca95ff6c27719
Lepec <n.lepec@student.isartdigital.com>
2020-10-06 09:59:32 +0200

    Base Project + gitignore

---
+++ ShooterMulti_base/Source/ShooterMulti/Characters/UndeadCharacter.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include "HealthCharacter.h"
+#include "UndeadCharacter.generated.h"
+
+UCLASS()
+class SHOOTERMULTI_API AUndeadCharacter : public AHealthCharacter
+{
+	GENERATED_BODY()
+
+private:
+
+	bool bHasPunched;
+
+	void PlayPunchMontage();
+
+public:
+
+	UPROPERTY(Category = UndeadCharacter, EditAnywhere, BlueprintReadWrite)
+	float PunchCooldown = 1.5f;
+
+	AUndeadCharacter();
+
+	virtual void BeginPlay() override;
+	void StartDisapear() override;
+	void Reset() override;
+	bool Punch();
+};
